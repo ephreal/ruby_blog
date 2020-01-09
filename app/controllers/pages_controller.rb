989cef89c004a3e4796d33515b6d7f13a831ca48
ephreal <tjannx@hotmail.com>
2020-01-09 14:58:53 -0700

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+
+    def index()
+       @pages = Page.all 
+    end
+
+    def show()
+        @page = Page.find(params[:id])
+    end
+
+    def new()
+       @page = Page.new()
+    end
+
+    # POST requests
+    def create()
+        render plain: params.to_json
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :posts
+  get '/pages', to: 'pages#index'
+  get '/pages/new', to: 'pages#new', as: :create_new_page
+  get '/pages/:id', to: 'pages#show', as: 'page'
+
+  post '/pages', to: 'pages#create'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
